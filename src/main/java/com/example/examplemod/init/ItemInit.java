3c0c1397c5dcbd09e46921f9930a10ac6852bda0
Moopheus <145939096+Moopheus@users.noreply.github.com>
2023-12-15 11:30:43 +0900

    Update ItemInit.java

--- src/main/java/com/example/examplemod/init/ItemInit.java
+++ src/main/java/com/example/examplemod/init/ItemInit.java
@@ -1,1 +1,16 @@
 
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.HoeItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.ShovelItem;
+import net.minecraft.world.item.SwordItem;
+
+public class itemInit {
+  public static final DeferredRegister<Item> ITEMS = 
+            DeferredRegister.create(ForgeRegistries.ITEMS, McExpander.MOD_ID);
+}

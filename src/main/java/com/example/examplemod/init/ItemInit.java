b27c48ef23b395d11c40acfddf33627a6eb0e9ac
Moopheus <145939096+Moopheus@users.noreply.github.com>
2023-12-15 17:10:23 +0900

    Update ItemInit.java

--- src/main/java/com/example/examplemod/init/ItemInit.java
+++ src/main/java/com/example/examplemod/init/ItemInit.java
@@ -1,4 +1,9 @@
-
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.AxeItem;
@@ -13,4 +18,6 @@ import net.minecraft.world.item.SwordItem;
 public class itemInit {
   public static final DeferredRegister<Item> ITEMS = 
             DeferredRegister.create(ForgeRegistries.ITEMS, McExpander.MOD_ID);
+  public static final RegistryObject<Item> CORRUPT_SHARDS = ITEMS.register("corrupt_shards",
+            () -> new Item(new Item.Properties()));
 }

ccb5654c174b65f0b55f951d841c348848b7bcc7
Moopheus <145939096+Moopheus@users.noreply.github.com>
2023-12-16 15:04:47 +0900

    Update FuelItem.java

--- src/main/java/com/example/mcexpander/items/FuelItem.java
+++ src/main/java/com/example/mcexpander/items/FuelItem.java
@@ -1,3 +1,12 @@
+package com.example.mcexpander.items;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+import net.minecraft.world.item.Item.Properties;
+import net.minecraft.world.item.crafting.RecipeType;
+
+import javax.annotation.Nullable;
+
 public class FuelItem extends Item {
     private final int burnTicks;
     public FuelItem(Properties properties, int burnTimeInTicks) {

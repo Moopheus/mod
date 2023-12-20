c15310087243e17a16bd4386629327a7aefa464b
Moopheus <145939096+Moopheus@users.noreply.github.com>
2023-12-20 17:10:52 +0900

    Update BlockInit.java

--- src/main/java/com/example/mcexpander/init/BlockInit.java
+++ src/main/java/com/example/mcexpander/init/BlockInit.java
@@ -1,1 +1,6 @@
+public class BlockInit {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, FirstModMain.MOD_ID);
 
+    public static final RegistryObject<Block> CORRUPT_COBBLESTONE = BLOCKS.register("corrupt_cobblestone",
+            () -> new Block());
+}

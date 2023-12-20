ee7aa181e3d772c445f06cd5b1cdb2b090da7c61
Moopheus <145939096+Moopheus@users.noreply.github.com>
2023-12-20 17:35:53 +0900

    Update BlockInit.java

--- src/main/java/com/example/mcexpander/init/BlockInit.java
+++ src/main/java/com/example/mcexpander/init/BlockInit.java
@@ -2,5 +2,5 @@ public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, FirstModMain.MOD_ID);
 
     public static final RegistryObject<Block> CORRUPT_COBBLESTONE = BLOCKS.register("corrupt_cobblestone",
-            () -> new Block());
+            () -> new Block(Block.Properties.of(Material.STONE).strength(4f, 1200f).requiresCorrectToolForDrops().lightLevel((state) -> 1));
 }

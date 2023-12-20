91e326a63502627b0404f33d32db821d9271229c
Moopheus <145939096+Moopheus@users.noreply.github.com>
2023-12-21 07:45:05 +0900

    Update BlockInit.java

--- src/main/java/com/example/mcexpander/init/BlockInit.java
+++ src/main/java/com/example/mcexpander/init/BlockInit.java
@@ -1,6 +1,18 @@
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, FirstModMain.MOD_ID);
 
     public static final RegistryObject<Block> CORRUPT_COBBLESTONE = BLOCKS.register("corrupt_cobblestone",
             () -> new Block(Block.Properties.of(Material.STONE).strength(4f, 1200f).requiresCorrectToolForDrops().lightLevel((state) -> 1));
+    @SubscribeEvent
+    public static void onRegisterItems(final RegisterEvent event) {
+        if (event.getRegistryKey().equals(ForgeRegistries.Keys.ITEMS)){
+            BLOCKS.getEntries().forEach( (blockRegistryObject) -> {
+                Block block = blockRegistryObject.get();
+                Item.Properties properties = new Item.Properties().tab(ItemInit.ModCreativeTab.instance);
+                Supplier<Item> blockItemFactory = () -> new BlockItem(block, properties);
+                event.register(ForgeRegistries.Keys.ITEMS, blockRegistryObject.getId(), blockItemFactory);
+            });
+        }
+    }
 }

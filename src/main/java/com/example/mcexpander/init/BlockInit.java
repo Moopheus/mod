4d8cbacb24147ebd5cfc44baf09d743539b574d5
Moopheus <145939096+Moopheus@users.noreply.github.com>
2023-12-21 07:54:42 +0900

    Update BlockInit.java

--- src/main/java/com/example/mcexpander/init/BlockInit.java
+++ src/main/java/com/example/mcexpander/init/BlockInit.java
@@ -1,3 +1,16 @@
+package com.example.mcexpander.init;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegisterEvent;
+import net.minecraftforge.registries.RegistryObject;
+import java.util.function.Supplier;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, FirstModMain.MOD_ID);
